dc33aa4b25a98974cddb5c2f456e5092e09576e6
Fumiko Yamamoto <fumi811bbc@gmail.com>
2016-01-23 17:47:13 +0900

    Initial Commit

---
+++ SKOctagonSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SKOctagonSample
+//
+//  Created by FumikoYamamoto on 2016/01/23.
+//  Copyright © 2016年 FumikoYamamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SKOctagonSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SKOctagonSample
+//
+//  Created by FumikoYamamoto on 2016/01/23.
+//  Copyright © 2016年 FumikoYamamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
